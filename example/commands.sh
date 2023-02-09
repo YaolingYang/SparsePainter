9f64bc4ff0172c9a34145763a6634e855b7762a1
Daniel Lawson <dan.lawson@bristol.ac.uk>
2023-02-09 21:45:56 +0000

    some improvements to commands, still incomplete

--- example/commands.sh
+++ example/commands.sh
@@ -43,11 +43,59 @@ for i in `seq 1 1000`; do echo "$i p_target 1" >> popnames.ids; done
 
 # Longmatchquery -- find matches longer than L.
 ${bindir}./exelmq_1swp_dpbwt$exe -i p_donor.vcf -q p_target.vcf -m -L 1 -o target_match.txt
-${bindir}./exelmq_1swp_dpbwt$exe -i p_donor.vcf -q p_donor.vcf -m -L 1 -o donor_match.txt
+
+
+exe=""
+bindir="../d-PBWT-longmatchquery/"
+awk="mawk" # awk is more available but slower
+data="data/"
+matches="matches/"
+
+if [ "$OS" = "Windows_NT" ]; then
+    detected_OS="Windows"
+    exe=".exe"
+    bindir="..\\d-PBWT-longmatchquery\\"
+else
+    detected_OS=`uname 2>/dev/null || echo Unknown`
+fi
+echo "Using OS $detected_OS"
+plink="plink1.9"
+mkdir -p $matches
+mkdir -p $data
+
+## 
+for i in {1..3}; do  bcftools index --force p${i}.vcf.gz; done
+
+# Use bcftools isec to find overlapping SNPs
+bcftools isec -n=3 p1.vcf.gz p2.vcf.gz p3.vcf.gz -o ${data}common.vcf
+
+# Use bcftools view to extract overlapping SNPs from the original .vcf.gz files
+for i in {1..3}; do bcftools view -R ${data}common.vcf p${i}.vcf.gz > ${data}p${i}_common.vcf; done
+
+for i in {1..3}; do bgzip -k -f ${data}p${i}_common.vcf; tabix ${data}p${i}_common.vcf.gz; done
+
+for i in {1..3}; do bcftools norm -d all ${data}p${i}_common.vcf.gz | bgzip -f > ${data}p${i}_common.bak.vcf.gz; mv ${data}p${i}_common.bak.vcf.gz ${data}p${i}_common.vcf.gz; tabix -f ${data}p${i}_common.vcf.gz; done
+
+for i in {1..3}; do gunzip -f -k ${data}p${i}_common.vcf.gz > ${data}p${i}_common.vcf ; done
+
+bcftools merge ${data}p1_common.vcf.gz ${data}p2_common.vcf.gz  --force-samples -o ${data}p_donor.vcf
+
+mv ${data}p3_common.vcf ${data}p_target.vcf
+
+for i in `seq 1 40`; do echo "p1:$i p1 1" > ${data}popnames.ids; done
+for i in `seq 1 40`; do echo "p2:$i p2 1" >> ${data}popnames.ids; done
+for i in `seq 1 1000`; do echo "$i p_target 1" >> ${data}popnames.ids; done
+
+# The above generates the required input to the software
+# The below begins running the software, with the input data of p_donor.vcf, p_target.vcf and popnames.ids
 
 # we need the map showing the recombination distance
-plink --vcf p_donor.vcf --recode --out p
+$plink --vcf ${data}p_donor.vcf --recode --out ${data}p
+
+# Longmatchquery -- find matches longer than L.
+${bindir}./lmq_1swp_dpbwt$exe -i ${data}p_donor.vcf -q ${data}p_target.vcf -m -L 1 -o ${matches}target_match.txt
+${bindir}./lmq_1swp_dpbwt$exe -i ${data}p_donor.vcf -q ${data}p_donor.vcf -m -L 1 -o ${matches}donor_match.txt
 
 # split it into a match file for a target/donor individual
-seq 0 999 | parallel --jobs 10 "grep -w q{} target_match.txt > target_match{}.txt"
-seq 0 79 | parallel --jobs 10 "grep -w q{} donor_match.txt > donor_match{}.txt"
+seq 0 999 | parallel --jobs 10 "grep -w q{} matches/target_match.txt > matches/target_match{}.txt"
+seq 0 79 | parallel --jobs 10 "grep -w q{} matches/donor_match.txt > matches/donor_match{}.txt"

c9b329f6fbee8511fb75c0eb9126662d67860d0b
Yaoling Yang <91947158+YaolingYang@users.noreply.github.com>
2023-04-17 15:30:29 +0100

    Update runcode.R

--- devel/runcode.R
+++ devel/runcode.R
@@ -2,7 +2,7 @@ library("Rcpp")
 Sys.setenv(PKG_CXXFLAGS = "-I.")
 Sys.setenv(PKG_LIBS = paste("-lz", paste0(normalizePath(getwd()), "/gzstream.o"), sep = " "))
 sourceCpp("hashmap.cpp", env = globalenv(), rebuild = TRUE, verbose = TRUE)
-setwd("bigeg2")
-a=paintingalldense(nind=50,targetfrac=1,fixrho=FALSE,method="Viterbi",targetfile="target.vcf.gz",
-                           donorfile="donor.vcf.gz",
-                           minmatchfrac = 0.002,L_initial=320,L_minmatch=20,window=0.05)
+
+a=paintingalldense(nind=50,targetfrac=1,fixrho=FALSE,method="Viterbi",
+                   targetfile="target.vcf.gz",donorfile="donor.vcf.gz",
+                   minmatchfrac = 0.002,L_initial=320,L_minmatch=20,window=0.05)

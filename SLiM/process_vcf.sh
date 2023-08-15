cbc1b84ec7a9d1e67a8e860357448eeb4ac9b7a1
Yaoling Yang <91947158+YaolingYang@users.noreply.github.com>
2023-08-15 18:52:05 +0100

    Update process_vcf.sh

--- SLiM/process_vcf.sh
+++ SLiM/process_vcf.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 bgzip p0.vcf
-bgzip p1.vcf
-bgzip p3.vcf
+tabix p0.vcf.gz
 
-bcftools index p0.vcf.gz
-bcftools index p1.vcf.gz
-bcftools index p3.vcf.gz
+bgzip p1.vcf
+tabix p1.vcf.gz
+
+bgzip p3.vcf
+tabix p3.vcf.gz
+
+bcftools view -O z --exclude 'MAF<0.01' p0.vcf.gz -o p0.vcf.gz.tmp && mv p0.vcf.gz.tmp p0.vcf.gz && tabix p0.vcf.gz
+
+bcftools view -O z --exclude 'MAF<0.01' p1.vcf.gz -o p1.vcf.gz.tmp && mv p1.vcf.gz.tmp p1.vcf.gz && tabix p1.vcf.gz
+
+bcftools view -O z --exclude 'MAF<0.01' p3.vcf.gz -o p3.vcf.gz.tmp && mv p3.vcf.gz.tmp p3.vcf.gz && tabix p3.vcf.gz
 
 //find common SNPs and output them in output_dir/sites.txt
 bcftools isec -n=3 -p output_dir p0.vcf.gz p1.vcf.gz p3.vcf.gz
@@ -42,9 +49,16 @@ awk '{if (NR <= 500) print $1"\t0"; else print $1"\t1"}' refsamples.txt > sim_po
 //extract target samples and create sim_targetname.txt
 bcftools query -l sim_target.vcf > sim_targetname.txt
 
+cp sim_ref.vcf.gz sim_ref2.vcf.gz
+cp sim_target.vcf sim_target2.vcf
+bgzip sim_target.vcf
+
 //convert vcf to phase format
-pbwt -readVcfGT sim_ref.vcf.gz -writePhase sim_ref.phase
-pbwt -readVcfGT sim_target.vcf -writePhase sim_target.phase
+pbwt -readVcfGT sim_ref2.vcf.gz -writePhase sim_ref.phase
+pbwt -readVcfGT sim_target2.vcf -writePhase sim_target.phase
+
+rm sim_ref2.vcf.gz
+rm sim_target2.vcf
 
 //run HNPaint
 ./HMPaint.exe -reffile sim_ref.phase -targetfile sim_target.phase -popfile sim_popnames.txt -mapfile sim_map.txt -targetname sim_targetname.txt -L_minmatch 10 -matchfrac 0.01 -AAS 0 -out sim_2pop

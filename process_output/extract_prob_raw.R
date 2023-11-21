c1b194d6290cd517421984e34fd945803718112d
Yaoling Yang <91947158+YaolingYang@users.noreply.github.com>
2023-11-21 00:20:24 +0000

    Update extract_prob_raw.R

--- process_output/extract_prob_raw.R
+++ process_output/extract_prob_raw.R
@@ -15,3 +15,9 @@ for(i in 2:ncol(raw)){
   painting_temp_list <- lapply(painting_temp_list, function(x) 0.5*(x[1:26]/sum(x[1:26])+x[27:52]/sum(x[27:52])))
   painting[[i-1]] <- do.call(rbind, painting_temp_list)
 }
+
+array_data <- array(unlist(painting), dim = c(nind, npop, nsnp))
+
+painting_final <- lapply(1:npop, function(x) {
+  array_data[, x, ]
+})

74bea36a4660a192236915bd8ffb1c9369f7a865
Yaoling Yang <91947158+YaolingYang@users.noreply.github.com>
2023-11-21 11:25:28 +0000

    Update extract_prob_linear.R

--- process_output/extract_prob_linear.R
+++ process_output/extract_prob_linear.R
@@ -3,8 +3,8 @@ linear=fread("chr19_1000G_linear_10inds_prob.txt.gz",fill=TRUE)
 nsnp=as.numeric(linear[nrow(linear),SNPidx])
 npop=ncol(linear)-1
 split_linear=which(is.na(linear[,2]))
-split_linear=c(split_linear,nrow(linear)+1)
 nind=length(split_linear)/2
+split_linear=c(split_linear,nrow(linear)+1)
 SNPidx=1:nsnp
 
 
@@ -15,28 +15,38 @@ painting=lapply(1:nind,function(h){
   ii=2 # Index of the end of the current interval
   for(i in 1:length(SNPidx)){
     SNPid = SNPidx[i]
-    while(snppos[ii]<=SNPid){
+    while(snppos[ii]<=SNPid & snppos[ii]!=nsnp){
       ii = ii + 1
     }
-    prop=(SNPid - snppos[ii-1])/(snppos[ii]-snppos[ii-1])
-    start=as.numeric(linear[split_linear[2*h-1]+ii-1,-1])
-    end=as.numeric(linear[split_linear[2*h-1]+ii,-1])
-    prob=start + (end-start)*prop
-    painting_temp[,i] = 0.5*prob/sum(prob)
+    if(snppos[ii]==nsnp){
+      prob=as.numeric(linear[split_linear[2*h-1]+ii-1,-1])
+      painting_temp[,i] = 0.5*prob/sum(prob)
+    }else{
+      prop=(SNPid - snppos[ii-1])/(snppos[ii]-snppos[ii-1])
+      start=as.numeric(linear[split_linear[2*h-1]+ii-1,-1])
+      end=as.numeric(linear[split_linear[2*h-1]+ii,-1])
+      prob=start + (end-start)*prop
+      painting_temp[,i] = 0.5*prob/sum(prob)
+    }
   }
   
   snppos=as.numeric(unlist(linear[(split_linear[2*h]+1):(split_linear[2*h+1]-1),1]))
   ii=2
   for(i in 1:length(SNPidx)){
     SNPid = SNPidx[i]
-    while(snppos[ii]<=SNPid){
+    while(snppos[ii]<=SNPid & snppos[ii]!=nsnp){
       ii = ii + 1
     }
-    prop=(SNPid - snppos[ii-1])/(snppos[ii]-snppos[ii-1])
-    start=as.numeric(linear[split_linear[2*h]+ii-1,-1])
-    end=as.numeric(linear[split_linear[2*h]+ii,-1])
-    prob=start + (end-start)*prop
-    painting_temp[,i] = 0.5*prob/sum(prob)+painting_temp[,i]
+    if(snppos[ii]==nsnp){
+      prob=as.numeric(linear[split_linear[2*h]+ii-1,-1])
+      painting_temp[,i] = 0.5*prob/sum(prob)+painting_temp[,i]
+    }else{
+      prop=(SNPid - snppos[ii-1])/(snppos[ii]-snppos[ii-1])
+      start=as.numeric(linear[split_linear[2*h]+ii-1,-1])
+      end=as.numeric(linear[split_linear[2*h]+ii,-1])
+      prob=start + (end-start)*prop
+      painting_temp[,i] = 0.5*prob/sum(prob)+painting_temp[,i]
+    }
   }
   painting_temp
 })

633a48bff3e5bf09ff37569cde035bf6edd0f757
Yaoling Yang <91947158+YaolingYang@users.noreply.github.com>
2024-01-29 22:16:41 +0000

    Delete simulation directory

--- simulation/makemap.R
+++
@@ -1,5 +0,0 @@
-sim_map <- read.table("sim_map.txt", header = TRUE)
-sim_map$chromosome <- 1 
-sim_map$snp <- paste0("SNP_", 1:nrow(sim_map)) 
-sim_map <- sim_map[, c("chromosome", "snp", "gd", "pd")]
-write.table(sim_map, "sim.map", quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\t")
